5064e982fa622ab8c6dd551f231023c4f091cfa8
Mstemmer <stemmermerlin@gmail.com>
2022-12-05 10:54:10 +1300

    filtering vehicles working

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tourism_New_Zealand_Transport_App
  
-<h2>This application is part of my second Yoobee Summative assignment where we have to make an app where the user inputs the number of days and number of people on their trip and then recieve all vehicles that match their needs</h2>
+<h2>This application is part of my second Yoobee Summative assignment where we had to make an app where the user inputs the number of days and number of people on their trip and then recieve all vehicles that match their needs</h2>
 
 <p>made for the tourism new zealand brand</p>
--- javascript/results.js
+++ javascript/results.js
@@ -10,6 +10,7 @@ Object.defineProperty(String.prototype, 'capitalize', {
   let Vehicles = [
     {
         id:0,
+        name:"Very Super Fun Motorbike",
         vehicle:"motorbike",
         min_passenger:1,
         max_passenger:1,
@@ -22,6 +23,7 @@ Object.defineProperty(String.prototype, 'capitalize', {
     },
     {
         id:1,
+        name:"Car Small Model R2D2",
         vehicle:"small-car",
         min_passenger:1,
         max_passenger:2,
@@ -34,6 +36,7 @@ Object.defineProperty(String.prototype, 'capitalize', {
     },
     {
         id:2,
+        name:"Transporter AB42",
         vehicle:"large-car",
         min_passenger:1,
         max_passenger:5,
@@ -46,6 +49,7 @@ Object.defineProperty(String.prototype, 'capitalize', {
     },
     {
         id:3,
+        name:"Sleeper",
         vehicle:"motor-home",
         min_passenger:2,
         max_passenger:6,
@@ -76,7 +80,7 @@ results_location_span.innerHTML = pickupLocationValue.capitalize();
 let vehicles_container = $("#vehicles-container")[0];
 for(let vehicle of Vehicles) {
     let card = document.createElement("div");
-    card.classList.add("flex", "info-section-card");
+    card.classList.add("flex", "info-section-card", vehicle.vehicle);
     vehicles_container.appendChild(card)
 
     let vehicle_img_wrapper = document.createElement("div");
@@ -92,7 +96,7 @@ for(let vehicle of Vehicles) {
     card.appendChild(info_text_part);
 
     let vehicle_name = document.createElement("h2");
-    vehicle_name.innerHTML = "Toyota abc"
+    vehicle_name.innerHTML = vehicle.name;
     info_text_part.appendChild(vehicle_name);
 
     let vehicle_type = document.createElement("h3");
@@ -133,7 +137,7 @@ for(let vehicle of Vehicles) {
     hire_me_div.appendChild(hire_per_day_word);
 
     let hire_total_price = document.createElement("h3");
-    hire_total_price.innerHTML = vehicle.ppd * NumberOfDays;
+    hire_total_price.innerHTML = "$" + vehicle.ppd * NumberOfDays;
     hire_me_div.appendChild(hire_total_price);
 
     let hire_total_word = document.createElement("p");
@@ -146,10 +150,145 @@ for(let vehicle of Vehicles) {
     hire_me_div.appendChild(hire_me_button);
 }
 
-function update_prices(Vehicles) {
-    let hire_me_divs = $("hire-me-div")
+let motorbike_amount = document.getElementById("motorbikes-amount");
+let small_car_amount = document.getElementById("small-cars-amount");
+let large_car_amount = document.getElementById("large-cars-amount");
+let motor_home_amount = document.getElementById("motor-home-amount");
+update_allowed_vehicles();
+
+
+
+
+
+function update_allowed_vehicles() {
+    let info_section_cards = document.getElementsByClassName("info-section-card");
+
+    let motorbike_number = 0;
+    let small_car_number = 0;
+    let large_car_number = 0;
+    let motor_home_number = 0;
+
+    for(let card of info_section_cards) {
+
+        if(card.classList.contains("motorbike")) {
+            if(Vehicles[0].max_days < NumberOfDays || Vehicles[0].max_passenger < peopleInPartyValue || Vehicles[0].min_days > NumberOfDays || Vehicles[0].min_passenger > peopleInPartyValue) {
+                card.classList.add("hidden")
+            }
+            else {
+                motorbike_number ++;
+                card.classList.remove("hidden");
+            }
+        }
+
+        else if(card.classList.contains("small-car")) {
+            if(Vehicles[1].max_days < NumberOfDays || Vehicles[1].max_passenger < peopleInPartyValue || Vehicles[1].min_days > NumberOfDays || Vehicles[1].min_passenger > peopleInPartyValue) {
+                card.classList.add("hidden")
+            }
+            else {
+                small_car_number ++;
+                card.classList.remove("hidden");
+            }
+        }
+
+        else if(card.classList.contains("large-car")) {
+            if(Vehicles[2].max_days < NumberOfDays || Vehicles[2].max_passenger < peopleInPartyValue || Vehicles[2].min_days > NumberOfDays || Vehicles[2].min_passenger > peopleInPartyValue) {
+                card.classList.add("hidden")
+            }
+            else {
+                large_car_number ++;
+                card.classList.remove("hidden");
+            }
+        }
+
+        else if(card.classList.contains("motor-home")) {
+            if(Vehicles[3].max_days < NumberOfDays || Vehicles[3].max_passenger < peopleInPartyValue || Vehicles[3].min_days > NumberOfDays || Vehicles[3].min_passenger > peopleInPartyValue) {
+                card.classList.add("hidden")
+            }
+            else {
+                motor_home_number ++;
+                card.classList.remove("hidden");
+            }
+        }
+    }
+
+    if(motorbike_number !== 0) {
+        motorbike_amount.innerHTML = motorbike_number + " Motorbikes";
+        motorbike_amount.classList.remove("hidden");
+    }
+    else {
+        motorbike_amount.classList.add("hidden");
+    }
+
+    if(small_car_number !== 0) {
+        small_car_amount.innerHTML = small_car_number + " Small Cars";
+        small_car_amount.classList.remove("hidden");
+    }
+    else {
+        small_car_amount.classList.add("hidden");
+    }
+
+
+    if(large_car_number !== 0) {
+        large_car_amount.innerHTML = large_car_number + " Large Cars";
+        large_car_amount.classList.remove("hidden");
+    }
+    else {
+        large_car_amount.classList.add("hidden");
+    }
+
+
+    if(motor_home_number !== 0) {
+        motor_home_amount.innerHTML = motor_home_number + " Motor Homes";
+        motor_home_amount.classList.remove("hidden");
+    }
+    else {
+        motor_home_amount.classList.add("hidden");
+    }
+}
+
+
+
+function update_prices() {
+    let hire_me_divs = document.getElementsByClassName("hire-me-div")
     for(let div of hire_me_divs) {
         let vehicleObject = Vehicles[div.id];
-        div.children[2].innerHTML = vehicleObject.ppd * NumberOfDays
+        div.children[2].innerHTML = "$" + vehicleObject.ppd * NumberOfDays
+    }
+}
+
+let all_arrow_down = document.getElementsByClassName("arrow");
+let all_arrow_down_content = document.getElementsByClassName("arrow-content");
+
+for(let i=0; i<all_arrow_down.length; i++) {
+    all_arrow_down[i].onclick = function() {
+        all_arrow_down_content[i].classList.toggle("hidden");
+    }
+}
+
+let all_day_content_options = document.getElementsByClassName("day-item");
+for(let day of all_day_content_options) {
+    day.onclick = function() {
+        NumberOfDays = day.innerHTML;
+        results_days_span.innerHTML = NumberOfDays + " days";
+
+        for(let content of all_arrow_down_content) {
+            content.classList.add("hidden");
+        }
+        console.log(NumberOfDays)
+        update_prices();
+        update_allowed_vehicles();
+    }
+}
+
+let all_people_content_options = document.getElementsByClassName("people-item");
+for(let person of all_people_content_options) {
+    person.onclick = function() {
+        peopleInPartyValue = person.innerHTML;
+        results_people_span.innerHTML = peopleInPartyValue + " people";
+
+        for(let content of all_arrow_down_content) {
+            content.classList.add("hidden");
+        }
+        update_allowed_vehicles();
     }
 }
